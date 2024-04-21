5d8a8f9afcfba25f93bfc32c4bce4744de579713
wgsylcl <2116766237@qq.com>
2024-04-21 21:20:25 +0800

    test

--- src/maintool.cpp
+++ src/maintool.cpp
@@ -8,13 +8,3 @@ Q_INVOKABLE QString MainTool::getCurrentApplicationPath(void)
 {
     return QCoreApplication::applicationFilePath();
 }
-
-void MainTool::setpagetemp(QString newtemp)
-{
-    m_pagetemp = newtemp;
-}
-
-QString MainTool::getpagetemp(void) const
-{
-    return m_pagetemp;
-}
